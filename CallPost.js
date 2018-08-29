fc8636ac18f913c44fa3be648589c34f3c2ace42
icang69 <36757987+icang69@users.noreply.github.com>
2018-08-28 19:42:54 -0800

    Update CallPost.js

--- CallPost.js
+++ CallPost.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="post-1.html"}
-if(x=="2"){xxx="post-2.html"}
-if(x=="3"){xxx="post-3.html"}
-if(x=="4"){xxx="post-4.html"}
-if(x=="5"){xxx="post-5.html"}a_to_ve[i].href="https://www.safelink.ikhsan.my.id/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/p/post-1.html"}
+if(x=="2"){xxx="/p/post-2.html"}
+if(x=="3"){xxx="/p/post-3.html"}
+if(x=="4"){xxx="/p/post-4.html"}
+if(x=="5"){xxx="/p/post-5.html"}a_to_ve[i].href="https://www.safelink.ikhsan.my.id"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
